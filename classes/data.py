eef5c0146a8b7aac6eb728ca86436512c1dc9b5f
Chad-Mowbray <mowbray.chad@gmail.com>
2020-10-23 18:40:28 -0500

    change to dataclass

--- classes/data.py
+++ classes/data.py
@@ -1,5 +1,8 @@
 import re
+from dataclasses import dataclass, field
 
+
+@dataclass(frozen=True)
 class Data:
 
     def __new__(cls, *args):
@@ -7,22 +10,19 @@ class Data:
             cls.instance = super().__new__(cls)
         return cls.instance
 
-    variable_declarations = ["var", "let", "const"]
-    assignment = ["="]
-    integer = r'[0-9]+'
-    variable = r'^[^0-9{][a-zA-z0-9]*'
-    built_ins = {
-        "console.log": "print"
-    }
-    method_indicator = r'[.](?=.+\()' 
-    function_keyword = "function"
-    open_bracket = "{"
-    comment = "//"
-    math_operators = ["+", "-", "*", "/"]
-    for_operator = r'^for'
-    plus_plus = r'\+\+$'
-    list_bracket = r'^['
-    length = r'.length$'
-    colon = ":"
-    object_property = f'{variable}[.][a-zA-z0-9]+$'
-
+    variable_declarations: list = field(default_factory=lambda: ["var", "let", "const"])
+    assignment: list = field(default_factory=lambda: ["="])
+    integer: str = r'[0-9]+'
+    variable: str = r'^[^0-9{][a-zA-z0-9]*'
+    built_ins: object = field(default_factory=lambda: {"console.log": "print"})
+    method_indicator: str = r'[.](?=.+\()' 
+    function_keyword: str = "function"
+    open_bracket: str = "{"
+    comment: str = "//"
+    math_operators: list = field(default_factory=lambda: ["+", "-", "*", "/"])
+    for_operator: str = r'^for'
+    plus_plus: str = r'\+\+$'
+    list_bracket: str = r'^['
+    length: str = r'.length$'
+    colon: str = ":"
+    object_property: str = f'{variable}[.][a-zA-z0-9]+$'
